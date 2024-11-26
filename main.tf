e721aa8c1b2874b4a33307c5390c6a7b85e9f8d5
prasannadevops <prasannadevopsr@gmail.com>
2024-11-26 13:57:10 +0000

    update

--- main.tf
+++ main.tf
@@ -1,18 +1,6 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-}
-
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-}
 resource "azurerm_resource_group" "gitlab-rg" {
   name     = "gitlab-rg"
   location = "East US"
-  provider = azurerm.production
-}
+  provider = azurerm.producation
+   
+  }
